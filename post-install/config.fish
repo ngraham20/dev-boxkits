2a653812ff814235fabf3c7ce2560d1b91e1d1a3
Nathaniel Graham <nathaniel.graham@protonmail.com>
2025-02-24 20:44:57 -0700

    added post installation justfile

---
+++ post-install/config.fish
@@ -0,0 +1,6 @@
+starship init fish | source
+zoxide init fish | source
+
+set -gx EDITOR /home/linuxbrew/.linuxbrew/bin/hx
+
+fish_add_path /usr/local/go/bin
--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,6 @@
 dnf update -y
 grep -v '^#' ./packages.list | xargs dnf install -y
 
-
 # install go
 curl -L https://go.dev/dl/go1.24.0.linux-amd64.tar.gz -o /tmp/go.tar.gz
 tar -C /usr/local -xzf /tmp/go.tar.gz
@@ -20,3 +19,6 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install kubectl
 brew install kind
 brew install openshift-cli
+
+# install starship
+brew install starship
