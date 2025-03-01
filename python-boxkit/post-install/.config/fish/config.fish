973200f7179250fe0636ee169dbcdc51328b3571
Nathaniel Graham <nathaniel.graham@protonmail.com>
2025-03-01 16:25:40 -0700

    added direnv fish hook

--- python-boxkit/post-install/.config/fish/config.fish
+++ python-boxkit/post-install/.config/fish/config.fish
@@ -1,5 +1,6 @@
 /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 starship init fish | source
 zoxide init fish | source
+direnv hook fish | source
 
 set -gx EDITOR /home/linuxbrew/.linuxbrew/bin/hx
